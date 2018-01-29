a56dc97ccc3672cd1a2c2240846a9a864a686510
zubair.davids <bairyd@gmail.com>
2018-01-29 21:37:38 +0200

    added consolidated method to show example of unit testing.

--- src/main/java/za/co/zubair/respository/ItemRepository.java
+++ src/main/java/za/co/zubair/respository/ItemRepository.java
@@ -1,6 +1,8 @@
 package za.co.zubair.respository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import za.co.zubair.model.Item;
 import za.co.zubair.model.TYPE;
 
@@ -8,11 +10,14 @@ import java.util.List;
 
 public interface ItemRepository extends CrudRepository<Item, String>{
 
-    Item getItemBySerialNumber(String serialNumber);
+    @Query("SELECT i.serialNumber, i.description, i.type, i.thumbnail FROM Item i WHERE DESCRIPTION LIKE CONCAT('%',:description,'%')")
+    List<Item> getItemsMatchingDescription(String description);
 
-    List<Item> getItemsByDescription(String description);
+    @Query("SELECT i.serialNumber, i.description, i.type, i.thumbnail FROM Item i WHERE TYPE LIKE CONCAT('%',:type,'%')")
+    List<Item> getItemsByType(@Param("type") TYPE type);
 
-    List<Item> getItemsByType(TYPE type);
+    @Override
+    Item findOne(String serialNumber);
 
     @Override
     List<Item> findAll();
--- src/main/java/za/co/zubair/service/ItemService.java
+++ src/main/java/za/co/zubair/service/ItemService.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 
 public interface ItemService {
+    List<Item> getItem(String serialNumber, String description, TYPE type);
+
     Item getItemBySerialNumber(String serialNumber);
 
     List<Item> getItemsByDescription(String description);
